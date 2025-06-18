d651dbded74a7e1c8ce5a1703c7bca4d9aa9655d
Maria Frosini <maria.frosini@tum.de>
2025-06-18 14:52:13 +0200

    task 2

--- Lab03/Lab03_Matlab_Signal_Processing/Lab03_Matlab_Signal_Processing.m
+++ Lab03/Lab03_Matlab_Signal_Processing/Lab03_Matlab_Signal_Processing.m
@@ -5,19 +5,23 @@ clc, clearvars, close all
 %% 1) Read Data
 % Load the provided signal 'signal.csv' using the 'csvread()' function and
 % split the signal and time into two separate vectors.
-
+data =readmatrix('signal3.xlsx');
 fprintf('1) Start.\n')
 
-%{ 
-    Add your code here
-%}
+signal= data(:,2);
+time= data(:,1);
+
 
 % Plot the signal
 figure(1)
+plot(time,signal);
+xlabel('time [s]');
+ylabel('signal');
+xlim([0 max(time)]);
+grid on
+hold on
 
-%{ 
-    Add your code here
-%}
+
 
 fprintf('1) Done.\n')
 
@@ -27,14 +31,19 @@ fprintf('1) Done.\n')
 % Matlab. The 'doc' and 'help' functions might be useful to obtain detailed
 % information.
 
-% 2) First, calculate the required filter order and cutoff frequency and
-% print the result.
+
 
 fprintf('2) Start.\n')
 
-%{ 
-    Add your code here
-%}
+% designing a Butterworth digital filter
+Wp= 30/2500; %pass band freq
+Ws= 200/2500; %stop band freq
+Ap= 3; %pass band attenuation
+As= 30; %stopband attenuation
+
+[n, Wn] = buttord(Wp, Ws, Ap, As);
+disp(n);
+disp(Wn);
 
 fprintf('2) Done.\n')
 
@@ -43,11 +52,14 @@ fprintf('2) Done.\n')
 % original signal. Make sure to add a title, the axis descriptions, and a
 % legend.
 
+
 fprintf('3) Start.\n')
 
-%{ 
-    Add your code here
-%}
+[b, a] = butter(n, Wn); 
+filtered_signal = filter(b, a, signal);
+
+plot(time,filtered_signal);  % the shift is because of the order of the filter, increasing the order will increase the phase shift
+legend('Raw Signal', 'Filtered Signal');
 
 fprintf('3) Done.\n')
 
@@ -58,36 +70,49 @@ fprintf('4) Start.\n')
 
 % 4.1) First, obtain the length of the original and filtered signal and 
 % calculate their means. Print both mean values.
-
-%{ 
-    Add your code here
-%}
+l_o= length(signal);
+l_f= length(filtered_signal);
+mean_original =mean(signal);
+mean_filtered= mean(filtered_signal);
+disp(mean_original);
+disp(mean_filtered);
 
 
 % 4.2) Do the FFT for both signals; see the docs and lecture slides for
-% help. Make sure to remove the mean from the signals.
+% help. Make sure to remove the mean from the signals. THIS IS VERY
+% IMPORTANT BECAUESE IT WILL HIDE THE IMPORTANT INFO AND WE WILL ONLY SEE A
+% PEACK AT f= 0 Hz
 
-% 4.2.1.) Original signal
 
-%{ 
-    Add your code here
-%}
 
-% 4.2.2) Filtered signal
 
-%{ 
-    Add your code here
-%}
+FT_signal = fftshift(abs(fft(signal-mean_original)));
+
+
+FT_filtered = fftshift(abs(fft(filtered_signal-mean_filtered)));
+
 
 
 % 4.2.3) When plotting, only visualize the spectrum of to 500 Hz.
 
+Fs =5000; %sampling freq
+N = length(signal);  % Number of samples
+f = (-N/2:N/2-1)*(Fs/N);  % Frequency axis in Hz
+
+
+
 
 figure(2)
+plot(f,FT_signal);
+xlabel('frequency');
+xlim([-250 250]);
+ylabel('FFT');
+grid on
+hold on
+plot(f,FT_filtered);
+legend('Raw Signal', 'Filtered Signal');
 
-%{ 
-    Add your code here
-%}
+
 
 fprintf('4) Done.\n')
 
@@ -100,17 +125,37 @@ fprintf('4) Done.\n')
 fprintf('5) Start.\n')
 
 % 5.2) What is the frequency of they signal you have analyzed?
+%sig_raw= signal- mean_original;
+sig_clean= filtered_signal-mean_filtered;
 
-%{ 
-    Add your code here
-%}
+[a, f]= detect_signal_freq(sig_clean,Fs);
+disp(a);
+disp(f);
+
 
 fprintf('5) Done.\n')
 
-% 5.1) Define function
+function [peak_amp, peak_freq] =detect_signal_freq(signal, f_sampling)
+  
+  fft_signal = fft(signal(1:end-1)); % odd sized signal: (1:end-1)
+  amplitude = abs(fft_signal/length(fft_signal));
+  window = amplitude(1:length(fft_signal)/2+1); %only positive frequencies
+  window(2:end-1) = 2*window(2:end-1);
+  frequencies = f_sampling*(0:(length(fft_signal)/2))/length(fft_signal);
 
-%{ 
-    Add your code here
-%}
 
+  [peak_amp, idx] = max(window(2:end));
+  idx = idx + 1;
+  peak_freq = frequencies(idx);
+
+ 
+
+  figure(3)
+  plot(frequencies, window);
+  xlabel('frequency [Hz]');
+  ylabel('Aplitude');
+  grid on
+
+    
+end
  
